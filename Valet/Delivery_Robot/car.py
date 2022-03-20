891352bf7354cabfa132b20b9d8140edbce9d771
wokeengineer <98420717+wokeengineer@users.noreply.github.com>
2022-03-20 15:00:29 -0400

    delivery bot updated

--- Valet/Delivery_Robot/car.py
+++ Valet/Delivery_Robot/car.py
@@ -9,7 +9,7 @@ BLUE = (0,0,255)
 
 
 class CAR:
-    def __init__(self,max_x = 600,max_y = 600,pos =(0,0),angle = 0,r = 30,L = 20,height = 20,width = 30) -> None:
+    def __init__(self,max_x = 600,max_y = 600,pos =(0,0),angle = 0,r = 10,L = 20,height = 20,width = 30) -> None:
         self.pos = pos
         self.angle = angle
         self.x = pos[0]
@@ -24,7 +24,8 @@ class CAR:
         self.surf = pygame.image.load('Delivery_Bot.png')
         self.surf = pygame.transform.scale(self.surf,(width,height))
 
-    def change_state(self,x,y,angle):
+    def change_state(self,state):
+        x,y,angle = state
         self.x = x
         self.y = y
         self.pos = (x,y)
@@ -35,15 +36,19 @@ class CAR:
 
 
     def next_state(self,vel_l,vel_r):
-        delta_x = self.r*(vel_r + vel_l)*cos(radians(-self.angle))/2
-        delta_y = self.r*(vel_r + vel_l)*sin(radians(-self.angle))/2
-        delta_angle = self.r*10*(vel_r-vel_l)/self.L
+        delta_x = self.r*(vel_r + vel_l)*cos(radians(-self.angle))/4
+        delta_y = self.r*(vel_r + vel_l)*sin(radians(-self.angle))/4
+        delta_angle = self.r*30*(vel_r-vel_l)/self.L
         x = self.x + delta_x
         y = self.y + delta_y
         angle =(self.angle +delta_angle)%360
         return (x,y,angle)
 
-    
+    def ref_rect(self,x,y,angle):
+        surf = pygame.transform.rotate(self.surf,angle = angle)
+        rect1 = surf.get_rect()
+        rect1.center = (x + 10*cos(radians(-angle)),y+ 10*sin(radians(-angle)))
+        return rect1
     def draw(self,win):
     
         surf = pygame.transform.rotate(self.surf,angle = self.angle)
@@ -64,10 +69,17 @@ if __name__ == '__main__':
     win = pygame.display.set_mode((width_win,width_win))
     pygame.display.set_caption("Title")
 
-    car = CAR(pos=(300,300))
+
+
+    car = CAR(pos=(30,30))
+    obstacle1 = pygame.Rect(250,250,60,60)
+    obstacle2 = pygame.Rect(50,550,30,20)
     run = True
     while run:
         win.fill(WHITE)
+        
+        pygame.draw.rect(win,RED,obstacle1)
+        pygame.draw.rect(win,RED,obstacle2)
         events = pygame.event.get()
         for ev in events:
             if ev.type == pygame.QUIT:
@@ -86,8 +98,14 @@ if __name__ == '__main__':
                     
                 if ev.key == pygame.K_d:
                     x,y,angle = car.next_state(1,-1)
-                print(angle)
+                
+                col = pygame.Rect.colliderect(car.ref_rect(x,y,angle),obstacle1)
+                print(col)
+                if col:
+                    x,y,angle = car.get_state()
+                    
             car.change_state(x,y,angle)
+            
         car.draw(win)
         pygame.display.update()
 
