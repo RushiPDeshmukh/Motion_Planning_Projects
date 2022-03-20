7fb034a4178a16aea3ad53ecf07f681a513a72ea
wokeengineer <98420717+wokeengineer@users.noreply.github.com>
2022-03-19 20:41:41 -0400

    modified car files

--- Valet/Car/car.py
+++ Valet/Car/car.py
@@ -1,28 +1,29 @@
-from turtle import width
 import pygame
 import numpy as np
 from math import *
 
-RED = (255,0,0)
+RED = (255,100,0)
 WHITE = (255,255,255)
 BLUE = (0,0,255)
+BLACK = (0,0,0)
 
 
 class CAR:
-    def __init__(self,max_x = 600,max_y = 600,pos =(0,0),angle = 0,r = 30,L = 20,height = 20,width = 30) -> None:
+    def __init__(self,max_x = 600,max_y = 600,pos =(0,0),angle = 0,steering_angle = 0,height = 20,width = 30) -> None:
         self.pos = pos
         self.angle = angle
         self.x = pos[0]
         self.y = pos[1]
         self.height = height
         self.width = width 
+        self.L = 20
     
-        self.r = r
-        self.L = L
+        self.steering_angle = steering_angle
         self.max_x = max_y
         self.max_y = max_y
-        self.surf = pygame.image.load('Delivery_Bot.png')
+        self.surf = pygame.image.load('car.png')
         self.surf = pygame.transform.scale(self.surf,(width,height))
+        self.surf.set_colorkey(WHITE)
 
     def change_state(self,x,y,angle):
         self.x = x
@@ -34,21 +35,24 @@ class CAR:
         return (self.x,self.y,self.angle)
 
 
-    def next_state(self,vel_l,vel_r):
-        delta_x = self.r*(vel_r + vel_l)*cos(radians(-self.angle))/2
-        delta_y = self.r*(vel_r + vel_l)*sin(radians(-self.angle))/2
-        delta_angle = self.r*10*(vel_r-vel_l)/self.L
+    def next_state(self,vel,steer):
+        delta_x = vel*cos(radians(-self.angle))
+        delta_y = vel*sin(radians(-self.angle))
+        delta_angle = degrees(vel*tan(radians(steer))/self.L)
+        self.steering_angle = steer
         x = self.x + delta_x
         y = self.y + delta_y
-        angle =(self.angle +delta_angle)%360
+        angle =(self.angle+delta_angle)%360
         return (x,y,angle)
+        
 
     
     def draw(self,win):
     
         surf = pygame.transform.rotate(self.surf,angle = self.angle)
+
         rect1 = surf.get_rect()
-        rect1.center = (self.pos[0] + 10*cos(radians(-self.angle)),self.pos[1]+ 10*sin(radians(-self.angle)))
+        rect1.center = self.pos
 
         
         win.blit(surf,rect1)
@@ -76,16 +80,16 @@ if __name__ == '__main__':
             x,y,angle = car.get_state()
             if ev.type == pygame.KEYDOWN:
                 if ev.key == pygame.K_w:
-                    x,y,angle = car.next_state(1,1)
+                    x,y,angle = car.next_state(10,0)
                     
                 if ev.key == pygame.K_s:
-                    x,y,angle = car.next_state(-1,-1)
+                    x,y,angle = car.next_state(-10,0)
                     
                 if ev.key == pygame.K_a:
-                    x,y,angle = car.next_state(-1,1)
+                    x,y,angle = car.next_state(5,30)
                     
                 if ev.key == pygame.K_d:
-                    x,y,angle = car.next_state(1,-1)
+                    x,y,angle = car.next_state(5,-30)
                 print(angle)
             car.change_state(x,y,angle)
         car.draw(win)
